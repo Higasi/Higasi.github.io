eeb7597a6024b77ca7bc0a0857cbd10c38918edd
Higasi <ramirezhiga@live.com.mx>
2016-03-09 11:01:28 -0600

    Update ejemplo09-03-16.2.js

--- rv/ejemplo09-03-16.2.js
+++ rv/ejemplo09-03-16.2.js
@@ -2,7 +2,7 @@
 function setup(){
 
 var forma= new THREE.BoxGeometry (1,1,1);
-var material= new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material= new THREE.MeshNormalMaterial({color: 0x00ff00});
 malla = new THREE.Mesh(forma, material);
 
 escena= new THREE.Scene();
