4ddb5444386cf1cc5dcb612ec1673858562b6dfa
Higasi <ramirezhiga@live.com.mx>
2016-04-13 11:05:28 -0500

    Create agents.js

---
+++ rv/agents.js
@@ -0,0 +1,42 @@
+function Agent (x=0,y=o){
+THREE.Object3D.call(this);
+this.position.x=x;
+this.position.y=y;
+}
+
+Agent.prototype= new THREE.Object3D();
+
+Agent.prototype.sense= function (environment){};
+Agent.prototype.plan= function (environment){};
+Agent.prototype.act= function (environment){};
+
+function Environment (){
+
+THREE.Scene.call(this);
+}
+Environment.prototype= new THREE.Scene();
+
+Environment.prototype.sense = function(){
+fir(var i=0; i<this.children.lenght;i++){
+if(this.children[i].sense !== undefined)
+this.children[i].sense(this);
+}
+}
+
+
+Environment.prototype.plan = function(){
+fir(var i=0; i<this.children.lenght;i++){
+if(this.children[i].sense !== undefined)
+this.children[i].plan(this);
+}
+}
+
+
+
+Environment.prototype.act = function(){
+fir(var i=0; i<this.children.lenght;i++){
+if(this.children[i].sense !== undefined)
+this.children[i].act(this);
+}
+}
+
