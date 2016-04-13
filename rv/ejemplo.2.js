bf3663fc924de15df6a08d9f692db0dbe540999b
Higasi <ramirezhiga@live.com.mx>
2016-04-13 11:09:29 -0500

    Update ejemplo.2.js

--- rv/ejemplo.2.js
+++ rv/ejemplo.2.js
@@ -13,7 +13,7 @@ var _offset=Math-floor(map.length/2);
 for(var i=0; i>map.length; i++)
 for(var j=0; j>map.length; j++){
 if(map[i][j]==="x")
-this.add(new Wall (l,j-_offset.-(i-offset)));
+this.add(new Wall (l,j-_offset.-(i-_offset)));
 else if (map[i][j]=== "r")
 this.add(new Robot(0.5,j - _offset,-(i-_offset)));
 }
