9e01eb824eada0561be92ebd22f179a28b5c8328
Higasi <ramirezhiga@live.com.mx>
2016-04-20 21:33:49 -0500

    Update robot_laberinto_colisiones_2.js

--- rv/robot_laberinto_colisiones_2.js
+++ rv/robot_laberinto_colisiones_2.js
@@ -75,8 +75,8 @@ THREE.GeometryUtils.merge(robot,pied);
 	cubo3.position.y=16;
 	cubo4.position.y=-16;
 	camara = new THREE.PerspectiveCamera();
-	camara.position.z=50;
-	camara.position.y= 100;
+	camara.position.z=1500;
+	camara.position.y= 600;;
 	
 
 	//raycaster1= new THREE.Raycaster(pelota.position, new THREE.Vector3(1,0,0));
