368341e23472ca48fd62e62036ba1baac58b4c16
Higasi <ramirezhiga@live.com.mx>
2016-04-22 13:31:14 -0500

    Create luz_puntual.js

---
+++ rv/luz_puntual.js
@@ -0,0 +1,39 @@
+function setup() 
+{
+THREE.ImageUtils.crossOrigin='';
+malla= new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshPhongMaterial({color:0xFF0000}));
+
+malla.position.y+=2;
+malla.poaition.z+=0.25;
+var base= new THREE.Mesh ( new THREE.BoxGeometry(5,.1,5), new THREE.MeshLambertMaterial({color: 0x00FF00}));
+
+var iluminacion = new THREE.AmbientLight(0xFFFFFF);
+escena= new THREE.Scene();
+escena.add(malla);
+escena.add(base);
+escena.add(iluminacion);
+
+
+camara= new THREE.PerspectiveCamera();
+camara.position.z=15;
+camara.position.y=5;
+renderer= new THREE.WebGLRenderer();
+
+renderer= setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+
+renderer.shadowMapEnabled=true;
+malla.castShadow= true;
+base.receiveShadow=true;
+iluminacion.castShadow=true;
+}
+function loop(){
+requestAnimationFrame(loop);
+malla.rotation.y+=0.01;
+
+renderer.render(escena, camara);
+}
+
+var camara, escena, renderer, malla;
+setup();
+loop();
