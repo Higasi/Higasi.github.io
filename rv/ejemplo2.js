5a28db8a2de389033c691ab8ff4d8a370141bc43
Higasi <ramirezhiga@live.com.mx>
2016-05-25 23:13:54 -0500

    Update ejemplo2.js

--- rv/ejemplo2.js
+++ rv/ejemplo2.js
@@ -5,15 +5,17 @@ function Wall(size,x,y){
   this.position.y=y;
 }
  
+ 
 Wall.prototype= new THREE.Mesh();
 Environment.prototype.setMap= function(map){
+  var esferaForma = new THREE.SphereGeometry(1);
   var _offset= Math.floor(map.length/2);
   for(var i=0; i< map.length; i++)
   for(var j=0; j< map.length; j++){
     if (map[i][j]==="x")
     this.add(new Wall(1,j-_offset,-(i-_offset)));
     else if (map[i][j]==="r")
-    this.add(new Robot(0.5,j-_offset,0));
+    this.add(new Robot(esferaForma,j-_offset,0));
   }
 }
 
