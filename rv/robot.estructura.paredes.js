66902f8400ce194d1b08a89a2267ccdf6a7c8d7c
Higasi <ramirezhiga@live.com.mx>
2016-05-31 09:03:14 -0500

    Update robot.estructura.paredes.js

--- rv/robot.estructura.paredes.js
+++ rv/robot.estructura.paredes.js
@@ -1,60 +1,4 @@
 
-function Llanta(angx,angy,angz){
- THREE.Object3D.call(this);
- THREE.ImageUtils.crossOrigin='';
- var textura  =THREE.ImageUtils.loadTexture('https://pansvinskys.github.io/rv/8712596-tire-texture-Stock-Photo-texture-truck-tires.jpg?raw=true');
- this.llanta=new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,1),new THREE.MeshBasicMaterial({color:0x3f3f3f,map:textura}));
- var rotx=new THREE.Matrix4().makeRotationX(angx);
- var roty=new THREE.Matrix4().makeRotationY(angy);
- var rotz=new THREE.Matrix4().makeRotationZ(angz);
- this.llanta.applyMatrix(rotx);
- this.llanta.applyMatrix(roty);
- this.llanta.applyMatrix(rotz);
- this.add(this.llanta);
-}
-
-function posicionLlanta(cosa,xp,yp,zp){
- cosa.position.x=xp;
- cosa.position.y=yp;
- cosa.position.z=zp;
-}
-
-function Cuerpo(){
- THREE.ImageUtils.crossOrigin='';
- var textura  =THREE.ImageUtils.loadTexture('https://pansvinskys.github.io/rv/metal-01.jpg?raw=true');
- THREE.Object3D.call(this);
- this.placa=new THREE.Mesh(new THREE.BoxGeometry(6,0.5,4),new THREE.MeshBasicMaterial({map:textura}));
- this.caja=new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshBasicMaterial({map:textura}));
- this.cam=new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,1),new THREE.MeshBasicMaterial({color:0x3f3f3f}));
- var rotcam= new THREE.Matrix4().makeRotationZ(Math.PI/2);
- this.placa.position.y=-2.5;
- this.caja.position.y=-1.1;
- this.cam.applyMatrix(rotcam);
- this.cam.position.y=0.3;
- this.add(this.placa);
- this.add(this.caja);
- this.add(this.cam);
-}
-
-function Robot(){
- THREE.Object3D.call(this);
- this.llanta1=new Llanta(Math.PI/2,0,0);
- this.llanta2=new Llanta(Math.PI/2,0,0);
- this.llanta3=new Llanta(Math.PI/2,0,0);
- this.llanta4=new Llanta(Math.PI/2,0,0);
- this.llanta5=new Llanta(Math.PI/2,0,0);
- this.llanta6=new Llanta(Math.PI/2,0,0);
- posicionLlanta(this.llanta1,-2,-3,2);
- posicionLlanta(this.llanta2,0,-3,2);
- posicionLlanta(this.llanta3,2,-3,2);
- posicionLlanta(this.llanta4,-2,-3,-2);
- posicionLlanta(this.llanta5,0,-3,-2);
- posicionLlanta(this.llanta6,2,-3,-2);
- this.cuerpobot=new Cuerpo();
- this.add(this.llanta1,this.llanta2,this.llanta3,this.llanta4,this.llanta5,this.llanta6);
- this.add(this.cuerpobot);
- 
-}
 
 function Sensor(position,direction){
  THREE.Raycaster.call(this,position,direction);
@@ -62,18 +6,81 @@ function Sensor(position,direction){
 }
 
 Sensor.prototype=new THREE.Raycaster();
-Llanta.prototype=new THREE.Object3D();
-Cuerpo.prototype=new THREE.Object3D();
-Robot.prototype=new THREE.Object3D();
+
+
 
 function Robots (x,y){
+ 
+ 	var material = new THREE.MeshNormalMaterial();
+
+
+	var brazoform = new THREE.BoxGeometry(0.2,0.2,3 );
+var cabezaform=new THREE.SphereGeometry(0.5);
+var cuerpoform=new THREE.BoxGeometry(1,1,3);
+var pieform=new THREE.BoxGeometry(0.5,0.2,0.2);
+var piernaform=new THREE.BoxGeometry(0.2,0.2,3);
+var manoform=new THREE.BoxGeometry(0.2,0.2,0.2);
+
+var cabeza= new THREE.Mesh(cabezaform,material);
+var cuerpo= new THREE.Mesh(cuerpoform,material);
+var pied=new THREE.Mesh(pieform,material);
+var piei= new THREE.Mesh(pieform,material);
+var piernad= new THREE.Mesh(piernaform,material);
+var piernai= new THREE.Mesh(piernaform,material);
+var brazoi=new THREE.Mesh(brazoform,material);
+var brazod=new THREE.Mesh(brazoform,material);
+var manoi=new THREE.Mesh(manoform,material);
+var manod=new THREE.Mesh(manoform,material);
+
+cuerpo.position.z=0;
+cabeza.position.z=2;
+piernai.position.z=-1;
+piernad.position.z=-1;
+piei.position.z=-2.5;
+piei.position.x=0.2;
+pied.position.z=-2.5;
+pied.position.x=0.2;
+brazoi.position.z=0;
+brazod.position.z=0;
+manoi.position.z=-0.2;
+manoi.position.x=0.2;
+manod.position.z=-0.2;
+manod.position.x=0.2;
+
+
+piernai.position.y=-.2;
+piernad.position.y=.2;
+piei.position.y=-.2;
+pied.position.y=.2;
+brazoi.position.y=-1;
+brazod.position.y=1;
+manoi.position.y=-1;
+manod.position.z=1;
+
+
+var robot= new THREE.Geometry();
+
+THREE.GeometryUtils.merge(robot,cabeza);
+THREE.GeometryUtils.merge(robot,cuerpo);
+THREE.GeometryUtils.merge(robot,brazoi);
+THREE.GeometryUtils.merge(robot,brazod);
+THREE.GeometryUtils.merge(robot,manoi);
+THREE.GeometryUtils.merge(robot,manod);
+THREE.GeometryUtils.merge(robot,piernai);
+THREE.GeometryUtils.merge(robot,piernad);
+THREE.GeometryUtils.merge(robot,piei);
+THREE.GeometryUtils.merge(robot,pied);
+
+ 
+ 
+ 
 Agent.call(this,x,y);
 this.sensor= new Sensor();
 this.sensor2= new Sensor();
 this.sensor3= new Sensor();
 this.sensor4= new Sensor();
 this.actuator= new Robot();
-idRobot=this.actuator;
+this.actuator= new THREE.Mesh(robot,material);
 this.actuator.rotation.x=Math.PI/2;
 this.actuator.commands=[];
 this.add(this.actuator);
