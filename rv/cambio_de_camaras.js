4d0f29f6d57386c8c6d6821da0f17d293f5aa953
Higasi <ramirezhiga@live.com.mx>
2016-05-31 01:02:58 -0500

    Update cambio_de_camaras.js

--- rv/cambio_de_camaras.js
+++ rv/cambio_de_camaras.js
@@ -1,102 +1,70 @@
-function Llanta(angx,angy,angz){
- THREE.Object3D.call(this);
- THREE.ImageUtils.crossOrigin='';
- //var textura  =THREE.ImageUtils.loadTexture('https://pansvinskys.github.io/rv/8712596-tire-texture-Stock-Photo-texture-truck-tires.jpg?raw=true');
- this.llanta=new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,1),new THREE.MeshLambertMaterial({color:0x3f3f3f,}));
- var rotx=new THREE.Matrix4().makeRotationX(angx);
- var roty=new THREE.Matrix4().makeRotationY(angy);
- var rotz=new THREE.Matrix4().makeRotationZ(angz);
- this.llanta.applyMatrix(rotx);
- this.llanta.applyMatrix(roty);
- this.llanta.applyMatrix(rotz);
- this.add(this.llanta);
-}
 
-function posicionLlanta(cosa,xp,yp,zp){
- cosa.position.x=xp;
- cosa.position.y=yp;
- cosa.position.z=zp;
-}
-
-function Cuerpo(){
- THREE.ImageUtils.crossOrigin='';
- var textura  =THREE.ImageUtils.loadTexture('https://pansvinskys.github.io/rv/metal-01.jpg?raw=true');
- THREE.Object3D.call(this);
- this.placa=new THREE.Mesh(new THREE.BoxGeometry(6,0.5,4),new THREE.MeshPhongMaterial({map:textura}));
- this.caja=new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshPhongMaterial({map:textura}));
- this.cam=new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,1),new THREE.MeshLambertMaterial({color:0x3f3f3f}));
- var rotcam= new THREE.Matrix4().makeRotationZ(Math.PI/2);
- this.placa.position.y=-2.5;
- this.caja.position.y=-1.1;
- this.cam.applyMatrix(rotcam);
- this.cam.position.y=0.3;
- this.add(this.placa);
- this.add(this.caja);
- this.add(this.cam);
-}
-
-function Robot(){
- THREE.Object3D.call(this);
- this.llanta1=new Llanta(Math.PI/2,0,0);
- this.llanta2=new Llanta(Math.PI/2,0,0);
- this.llanta3=new Llanta(Math.PI/2,0,0);
- this.llanta4=new Llanta(Math.PI/2,0,0);
- this.llanta5=new Llanta(Math.PI/2,0,0);
- this.llanta6=new Llanta(Math.PI/2,0,0);
- posicionLlanta(this.llanta1,-2,-3,2);
- posicionLlanta(this.llanta2,0,-3,2);
- posicionLlanta(this.llanta3,2,-3,2);
- posicionLlanta(this.llanta4,-2,-3,-2);
- posicionLlanta(this.llanta5,0,-3,-2);
- posicionLlanta(this.llanta6,2,-3,-2);
- this.cuerpobot=new Cuerpo();
- this.add(this.llanta1,this.llanta2,this.llanta3,this.llanta4,this.llanta5,this.llanta6);
- this.add(this.cuerpobot);
- 
-}
-
-function Obstaculos(){
- THREE.ImageUtils.crossOrigin='';
- var textura  =THREE.ImageUtils.loadTexture('https://pansvinskys.github.io/rv/wall-1033777_960_720.jpg?raw=true');
- THREE.Object3D.call(this);
- var mapa=[];
- this.cubo1={};
- mapa[0]=["x","x","x","x","x","x","x","x","x","x","x"];
- mapa[1]=["x","x","x","x","x","x","x","x","x","x","x"];
- mapa[2]=["x","x","x","x","x","x","x","x","x","x","x"];
- mapa[3]=["x","x","x","x","r","x","x","x","x","x","x"];
- mapa[4]=["x","x","x","x","x","x","x","x","x","x","x"];
- mapa[5]=["x","x","x","x","x","x","x","x","x","x","x"];
- for(j=0;j<=5;j++){
-  for(i=0;i<=10;i++){
-   if(mapa[j][i]=="x"){
-    this.cubo1[j*10+i]=new THREE.Mesh(new THREE.BoxGeometry(4,4,8),new THREE.MeshBasicMaterial({map:textura}));
-    this.cubo1[j*10+i].position.x=-20+4*i;
-    this.cubo1[j*10+i].position.z=-20+8*j;
-    this.add(this.cubo1[j*10+i]);
-   }
-   else if(mapa[j][i]=="0"){
-    
-   }
-   else if(mapa[j][i]=="r"){
-   this.rob=new Robot();
-   this.raycaster=new THREE.Raycaster(this.rob.position,new THREE.Vector3(1,0,0));
-   this.rob.position.x=-20+4*i;
-   this.rob.position.z=-20+8*j;
-   this.add(this.rob);
-   }
-   else{
-   }
-  }
- }
-}
-
-Llanta.prototype=new THREE.Object3D();
-Cuerpo.prototype=new THREE.Object3D();
-Robot.prototype=new THREE.Object3D();
-Obstaculos.prototype=new THREE.Object3D();
 
 function setup(){
+
+var material = new THREE.MeshNormalMaterial();
+
+
+	var brazoform = new THREE.BoxGeometry(0.2,0.2,3 );
+var cabezaform=new THREE.SphereGeometry(0.5);
+var cuerpoform=new THREE.BoxGeometry(1,1,3);
+var pieform=new THREE.BoxGeometry(0.5,0.2,0.2);
+var piernaform=new THREE.BoxGeometry(0.2,0.2,3);
+var manoform=new THREE.BoxGeometry(0.2,0.2,0.2);
+
+var cabeza= new THREE.Mesh(cabezaform,material);
+var cuerpo= new THREE.Mesh(cuerpoform,material);
+var pied=new THREE.Mesh(pieform,material);
+var piei= new THREE.Mesh(pieform,material);
+var piernad= new THREE.Mesh(piernaform,material);
+var piernai= new THREE.Mesh(piernaform,material);
+var brazoi=new THREE.Mesh(brazoform,material);
+var brazod=new THREE.Mesh(brazoform,material);
+var manoi=new THREE.Mesh(manoform,material);
+var manod=new THREE.Mesh(manoform,material);
+
+cuerpo.position.z=0;
+cabeza.position.z=2;
+piernai.position.z=-1;
+piernad.position.z=-1;
+piei.position.z=-2.5;
+piei.position.x=0.2;
+pied.position.z=-2.5;
+pied.position.x=0.2;
+brazoi.position.z=0;
+brazod.position.z=0;
+manoi.position.z=-0.2;
+manoi.position.x=0.2;
+manod.position.z=-0.2;
+manod.position.x=0.2;
+
+
+piernai.position.y=-.2;
+piernad.position.y=.2;
+piei.position.y=-.2;
+pied.position.y=.2;
+brazoi.position.y=-1;
+brazod.position.y=1;
+manoi.position.y=-1;
+manod.position.z=1;
+
+
+var robot= new THREE.Geometry();
+
+THREE.GeometryUtils.merge(robot,cabeza);
+THREE.GeometryUtils.merge(robot,cuerpo);
+THREE.GeometryUtils.merge(robot,brazoi);
+THREE.GeometryUtils.merge(robot,brazod);
+THREE.GeometryUtils.merge(robot,manoi);
+THREE.GeometryUtils.merge(robot,manod);
+THREE.GeometryUtils.merge(robot,piernai);
+THREE.GeometryUtils.merge(robot,piernad);
+THREE.GeometryUtils.merge(robot,piei);
+THREE.GeometryUtils.merge(robot,pied);
+
+
+	
+	
  document.addEventListener( 'keydown', onKeyDown, false );
  document.addEventListener( 'keyup', onKeyUp, false );
  var luzPuntual=new THREE.PointLight(0xffffff);
@@ -106,6 +74,7 @@ function setup(){
  obs=new Obstaculos();
  escena=new THREE.Scene();
  escena.add(luzPuntual,obs);
+ escena.add(robot);
  camara=new THREE.PerspectiveCamera();
  camara.position.y=50;
  renderer=new THREE.WebGLRenderer();
