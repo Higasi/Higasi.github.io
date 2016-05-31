570e5e883ca006f22ccfc3f5641a0804e31cc2d4
Higasi <ramirezhiga@live.com.mx>
2016-05-30 23:00:30 -0500

    Update cilindro_luz.js

--- rv/cilindro_luz.js
+++ rv/cilindro_luz.js
@@ -28,16 +28,17 @@ renderer= new THREE.WebGLRenderer();
 renderer. setSize( window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild( renderer.domElement);
 }
-/*
+
 function loop(){
 
 requestAnimationFrame (loop);
-
+/*
 malla.rotation.x +=0.01;
 malla.rotation.y +=0.01;
+*/
 renderer.render  ( escena, camara);
 }
-*/
+
 var camara, escena,  renderer, malla;
 
 setup();
