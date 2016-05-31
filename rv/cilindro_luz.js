d99022fb5ed154eb32b487e24a48890415a62b1e
Higasi <ramirezhiga@live.com.mx>
2016-05-30 22:32:40 -0500

    Update cilindro_luz.js

--- rv/cilindro_luz.js
+++ rv/cilindro_luz.js
@@ -3,28 +3,26 @@ function setup(){
 
 var forma= new THREE.BoxGeometry (1,1,5);
 var material = new THREE.MeshLambertMaterial( );
-var light1 = new THREE.PointLight( 0xff0040, 1, 50 );
+//var light1 = new THREE.PointLight( 0xff0040, 1, 50 );
 			
 			
 malla = new THREE.Mesh(forma, material);
-malla.material.transparent = 1;
+malla.material.transparent = 10000;
 malla.material.opacity=0.5;
   var LuzPuntual = new THREE.PointLight(0XFFFFFF,1000);
   malla.position.x=1;
     malla.position.y=1;
       malla.position.z=1;
-LuzPuntual.position.x =10;
-LuzPuntual.position.y =10;
-LuzPuntual.position.z =10;
+/*
 
 light1.position.x=1;
 light1.position.y=1;
 light1.position.z=1;
-
+*/
 escena= new THREE.Scene();
 escena.add(malla);
 escena.add(LuzPuntual); 
-	escena.add( light1 );
+	//escena.add( light1 );
 
 camara= new THREE.PerspectiveCamera();
 camara.position.z=10;
