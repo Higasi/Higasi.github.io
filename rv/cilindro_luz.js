b76e338c5141be3ee2e4a5a7ae9595a105df0f7e
Higasi <ramirezhiga@live.com.mx>
2016-05-30 22:53:11 -0500

    Update cilindro_luz.js

--- rv/cilindro_luz.js
+++ rv/cilindro_luz.js
@@ -23,7 +23,7 @@ escena= new THREE.Scene();
 	escena.add( light1 );
 
 camara= new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=50;
 renderer= new THREE.WebGLRenderer();
 renderer. setSize( window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild( renderer.domElement);
