e1e6811e07a75ee1db3f8999df54b0fc62c516ce
Higasi <ramirezhiga@live.com.mx>
2016-05-31 08:24:51 -0500

    Update robot.conjunto.paredes.js

--- rv/robot.conjunto.paredes.js
+++ rv/robot.conjunto.paredes.js
@@ -40,22 +40,22 @@ function setup(){
  var mapa=new Array();
  mapa[0] ="xxxxxxxxxxxxxxxxxxxxxxxx";
  mapa[1] ="x r      xxxxxxxxxxxxxxx";
- mapa[2] ="xxxxxxx  xxxxxxxxxxxxxxx";
- mapa[3] ="xxxxxxx  xxxxxxxxxxxxxxx"; 
- mapa[4] ="xxxxxxx      xxxxxxxxxxx";
- mapa[5] ="xxxxxxxx     xxxxxxxxxxx";
- mapa[6] ="xxxxxxxx         xxxx  x"; 
- mapa[7] ="xxxxxxxxx  xxxx  xxxx  x";
+ mapa[2] ="xxxxx    xxxxxxxxxxxxxxx";
+ mapa[3] ="xxxxx    xxxxxxxxxxxxxxx"; 
+ mapa[4] ="xxxxx        xxxxxxxxxxx";
+ mapa[5] ="xxxxx        xxxxxxxxxxx";
+ mapa[6] ="xxxxx                  x"; 
+ mapa[7] ="xxxxx      xxxx        x";
  mapa[8] ="xxx        xxxx        x";
- mapa[9] ="xxxxxxxxx   xxxxxxxxxxxx";
- mapa[10]="xxxx        xxxxxxx   xx"; 
- mapa[11]="xxxx  xxxxxxxxxxxxx   xx";
- mapa[12]="xxxx  xxxxxxx         xx";
- mapa[13]="xxxx  xxxxxxx   xxxxxxxx";
- mapa[14]="xxxx                   x";
+ mapa[9] ="xxx        xxxxxxxxxxxxx";
+ mapa[10]="xxx        xxxxxx     xx"; 
+ mapa[11]="xxx    xxxxxxxxxx     xx";
+ mapa[12]="xxx    xxxxx          xx";
+ mapa[13]="xxx    xxxxx        xxxx";
+ mapa[14]="xxx                    x";
  mapa[15]="x     xxxxxxxxxxxxxxxxxx";
  mapa[16]="x     xxxxxxxxxxxxxxxx x";
- mapa[17]="xxx      xxxxxxxxxxxxxxx";
+ mapa[17]="x     xxxxxxxxxxxxxxxxxx";
  mapa[18]="x                     g";
  mapa[19]="xxxxxxxxxxxxxxxxxxxxxxxx";
  mapa[20]="x                      x";
@@ -97,7 +97,7 @@ function onKeyUp ( event ) {
 function loop(){
  if(boton==true){
 	camara=new THREE.OrthographicCamera( 50/ - 2, 150 / 2, 160 / 2, 50 / - 2, 5, 200 );
-	camara.position.z=300
+	camara.position.z=1000;
 }
 else{
 	camara=new THREE.PerspectiveCamera;
