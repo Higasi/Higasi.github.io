af03eef5cc15cb9c9ce135ed04ca76d808606409
Higasi <ramirezhiga@live.com.mx>
2016-05-31 11:40:47 -0500

    Update cilindro_luz.js

--- rv/cilindro_luz.js
+++ rv/cilindro_luz.js
@@ -1,39 +1,45 @@
-
 function setup(){
+	var material = new THREE.MeshPhongMaterial({color: 0xD0A9F5});
+	material.transparent = true;
+	material.opacity =0;
+	malla= new THREE.Mesh(new THREE.CylinderGeometry(2,3,5),material);
+	
+	var pared = new THREE.Mesh(new THREE.BoxGeometry(8,8,.1),new THREE.MeshBasicMaterial({color: 0x0489B1}));
+	pared.position.z = -5;
+	
+	var iluminacion = new THREE.PointLight(0xffffff);
+	iluminacion.position.set(0,0,0);
 
-var forma= new THREE.BoxGeometry (1,1,5);
-//var material = new THREE.MeshLambertMaterial({opacity:0.6,transparent:true} );
-var light1 = new THREE.PointLight( 0xFFFFFF, 20, 50,2 );
-			
-//malla = new THREE.Mesh(forma, material);
-/*  malla.position.x=1;
-    malla.position.y=1;
-      malla.position.z=1;*/
-
-light1.position.set(0,0,0);
-escena= new THREE.Scene();
-//escena.add( new THREE.AmbientLight( 0xFFFFFF ) );
-//escena.add(malla);
-escena.add( light1 );
-camara= new THREE.PerspectiveCamera();
-camara.position.set(0,0,4);
-renderer= new THREE.WebGLRenderer();
-renderer. setSize( window.innerHeight*.95, window.innerHeight*.95);
-document.body.appendChild( renderer.domElement);
+	escena= new THREE.Scene();
+	escena.add(malla);
+	escena.add(pared);
+	escena.add(iluminacion);
+        escena.add(pared);
+	camara= new THREE.PerspectiveCamera();
+	camara.position.z=25;
+	renderer= new THREE.WebGLRenderer();
+	renderer.setSize(window.innerHeight*.75,window.innerHeight*.75);
+	document.body.appendChild(renderer.domElement);
+	
+	renderer.shadowMapEnabled = true;
+	malla.castShadow = true;
+	pared.receiveShadow = true;
+	iluminacion.castShadow = true;
+	step=0.2;
 }
 
 function loop(){
-
-requestAnimationFrame (loop);
-/*
-malla.rotation.x +=0.01;
-malla.rotation.y +=0.01;
-*/
-renderer.render  ( escena, camara);
+	requestAnimationFrame(loop);
+	if(malla.material.opacity >= 1)
+		step = -0.01;
+	if(malla.material.opacity <= 0)
+		step = 0.01;
+	
+	malla.material.opacity += step;
+	
+	renderer.render(escena,camara);
 }
 
-var camara, escena,  renderer, malla;
-
+var camara, escena, renderer, malla, step;
 setup();
 loop();
-
