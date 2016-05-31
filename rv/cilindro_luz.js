f06dd5ec7cad467ec6a77394f9127496d6037d91
Higasi <ramirezhiga@live.com.mx>
2016-05-30 22:53:57 -0500

    Update cilindro_luz.js

--- rv/cilindro_luz.js
+++ rv/cilindro_luz.js
@@ -3,7 +3,7 @@ function setup(){
 
 var forma= new THREE.BoxGeometry (1,1,5);
 var material = new THREE.MeshNormalMaterial({opacity:0.1,transparent:true} );
-var light1 = new THREE.PointLight( 0xff0040, 1000, 1000 );
+var light1 = new THREE.PointLight( 0xFFFFFF, 10, 50 );
 			
 			
 malla = new THREE.Mesh(forma, material);
